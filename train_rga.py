cdf29a2ecf6827ff8677c1fd6e9ee77c79bf81a1
Jason Li <148528698+Musketeer4@users.noreply.github.com>
2025-07-31 19:10:45 +0800

    Add files via upload

---
+++ train_rga.py
@@ -0,0 +1,85 @@
+from utils.logger import setup_logger
+from datasets.make_dataloader import make_dataloader
+from model.make_model_clipreid import make_model
+from solver.make_optimizer import make_optimizer
+from solver.scheduler_factory import create_scheduler
+from solver.lr_scheduler import WarmupMultiStepLR
+from processor.processor_rga import train_rga
+from model.make_model_clipreid import RGANet
+import random
+import torch
+import numpy as np
+import os
+import argparse
+from config import cfg
+import clip
+clip_model, _ = clip.load("ViT-B/32", device="cuda")
+
+
+torch.autograd.set_detect_anomaly(True)
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="ReID Baseline Training")
+    parser.add_argument(
+        "--config_file", default="configs/person/vit_clipreid.yml", help="path to config file", type=str
+    )
+
+    parser.add_argument("opts", help="Modify config options using the command-line", default=None,
+                        nargs=argparse.REMAINDER)
+    parser.add_argument("--local_rank", default=0, type=int)
+    args = parser.parse_args()
+
+    if args.config_file != "":
+        cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+
+    set_seed(cfg.SOLVER.SEED)
+
+    if cfg.MODEL.DIST_TRAIN:
+        torch.cuda.set_device(args.local_rank)
+
+    output_dir = cfg.OUTPUT_DIR
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    logger = setup_logger("transreid", output_dir, if_train=True)
+    logger.info("Saving model in the path :{}".format(cfg.OUTPUT_DIR))
+    logger.info(args)
+
+    if args.config_file != "":
+        logger.info("Loaded configuration file {}".format(args.config_file))
+        with open(args.config_file, 'r') as cf:
+            config_str = "\n" + cf.read()
+            logger.info(config_str)
+    logger.info("Running with config:\n{}".format(cfg))
+
+    if cfg.MODEL.DIST_TRAIN:
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
+    train_loader, train_loader_normal, val_loader, num_query, num_classes, camera_num, view_num = make_dataloader(cfg)
+
+    model = make_model(cfg, num_class=num_classes, camera_num=camera_num, view_num = view_num)
+    model = model.to("cuda") 
+    optimizer = make_optimizer(cfg, model)
+    scheduler = create_scheduler(optimizer, num_epochs = cfg.SOLVER.STAGE1.MAX_EPOCHS, lr_min = cfg.SOLVER.STAGE1.LR_MIN, \
+                        warmup_lr_init = cfg.SOLVER.STAGE1.WARMUP_LR_INIT, warmup_t = cfg.SOLVER.STAGE1.WARMUP_EPOCHS, noise_range = None)
+    train_rga( 
+        cfg,
+        model, 
+        train_loader,
+        val_loader,
+        optimizer,
+        scheduler,
+        num_query,
+        num_classes,
+        clip_model
+    )
